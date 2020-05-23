338443acdaa93601c8e02f7a083b8aee83b81be1
Greg Losco <gtlosco@gmail.com>
2020-05-22 17:40:49 -0700

    added new base URL

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,4 @@
-const baseURL = "http://localhost:3000"
+const baseURL = "https://fantasy-bbl-backend.herokuapp.com"
 const ownerURL = `${baseURL}/owners`
 const playerURL = `${baseURL}/players`
 const draftSlotContainers = document.querySelector("div#drafting-slots")
